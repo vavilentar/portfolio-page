836950523ba9fe3dd1d19b3b64165f330afcadb8
Roman Pchelkin <epos.91@mail.ru>
2023-04-17 13:34:39 +0300

    Refresh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const infoBoxes = document.querySelectorAll('.info-block');
 window.addEventListener('scroll', checkBoxes);
+const feedbackForm = document.getElementById('contacts__form');
 
 function checkBoxes() {
 	const triggerBottom = window.innerHeight / 5 * 4;
@@ -14,3 +15,20 @@ function checkBoxes() {
 		
 	});
 }
+
+$('form').submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: "../mailer/smart.php",
+		data: $(this).serialize()
+	}).done(function() {
+		$(this).find("input").val("");
+
+
+		$('form').trigger('reset');
+		feedbackForm.innerHTML = `<h2 style="font-size: 26px;">Спасибо за заявку! Я обазательно с Вами свяжусь!</h2>`
+	});
+
+	return false;
+});
